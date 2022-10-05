456272afc2bb492adb9117146ccd797f4f3b98ad
Ollie <ojimba01@gmail.com>
2022-10-05 14:07:27 -0400

    minor reconfiguration
    minor configuration tweaks in preparation of data that will be used in the SQL db

--- djproj/settings.py
+++ djproj/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crm',
+    'prm',
     'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
--- djproj/urls.py
+++ djproj/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('crm.urls')),
+    path('', include('prm.urls')),
     path('task/', include("tasks.urls"))
 ]
